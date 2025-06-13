fe4ea43984a8d95a75efab8b77a1394a321c2a44
paratonsp <paratonsp@gmail.com>
2025-06-14 01:59:03 +0700

    fix sitemap

--- src/routes/page/route.js
+++ src/routes/page/route.js
@@ -12,9 +12,85 @@ module.exports = {
       title: 'Website Sitemap',
       description: 'Website Sitemap of Page, Post and Everything in this website',
       content: `
-      <p>Elder.js is a bit different from most frameworks in that for pages to be statically generated it asks you to define all of your <span class="code">request</span> objects in your <span class="code">all()</span> function. While this step may seem a bit different from what you are used to it prevents Elder.js from having to crawl your site to render it which dramatically speeds up the build process.</p>
       <h2>What is Website Sitemap?</h2>
-      <p>Elder.js is a bit different from most frameworks in that for pages to be statically generated it asks you to define all of your <span class="code">request</span> objects in your <span class="code">all()</span> function. While this step may seem a bit different from what you are used to it prevents Elder.js from having to crawl your site to render it which dramatically speeds up the build process.</p>
+      <p>Website Sitemap of Page, Post and Everything in this website</p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-1000-ampere/">https://coveropini.my.id/acb-3-phase-1000-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-1250-ampere/">https://coveropini.my.id/acb-3-phase-1250-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-1600-ampere/">https://coveropini.my.id/acb-3-phase-1600-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-2000-ampere/">https://coveropini.my.id/acb-3-phase-2000-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-2500-ampere/">https://coveropini.my.id/acb-3-phase-2500-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-3200-ampere/">https://coveropini.my.id/acb-3-phase-3200-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-4000-ampere/">https://coveropini.my.id/acb-3-phase-4000-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-630-ampere/">https://coveropini.my.id/acb-3-phase-630-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-6300-ampere/">https://coveropini.my.id/acb-3-phase-6300-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-3-phase-800-ampere/">https://coveropini.my.id/acb-3-phase-800-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-1000-ampere/">https://coveropini.my.id/acb-4-phase-1000-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-1250-ampere/">https://coveropini.my.id/acb-4-phase-1250-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-1600-ampere/">https://coveropini.my.id/acb-4-phase-1600-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-2000-ampere/">https://coveropini.my.id/acb-4-phase-2000-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-2500-ampere/">https://coveropini.my.id/acb-4-phase-2500-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-3200-ampere/">https://coveropini.my.id/acb-4-phase-3200-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-4000-ampere/">https://coveropini.my.id/acb-4-phase-4000-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-630-ampere/">https://coveropini.my.id/acb-4-phase-630-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-6300-ampere/">https://coveropini.my.id/acb-4-phase-6300-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/acb-4-phase-800-ampere/">https://coveropini.my.id/acb-4-phase-800-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-10-ampere/">https://coveropini.my.id/mcb-1-phase-10-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-16-ampere/">https://coveropini.my.id/mcb-1-phase-16-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-2-ampere/">https://coveropini.my.id/mcb-1-phase-2-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-20-ampere/">https://coveropini.my.id/mcb-1-phase-20-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-25-ampere/">https://coveropini.my.id/mcb-1-phase-25-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-32-ampere/">https://coveropini.my.id/mcb-1-phase-32-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-4-ampere/">https://coveropini.my.id/mcb-1-phase-4-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-40-ampere/">https://coveropini.my.id/mcb-1-phase-40-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-50-ampere/">https://coveropini.my.id/mcb-1-phase-50-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-6-ampere/">https://coveropini.my.id/mcb-1-phase-6-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-1-phase-63-ampere/">https://coveropini.my.id/mcb-1-phase-63-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-10-ampere/">https://coveropini.my.id/mcb-2-phase-10-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-16-ampere/">https://coveropini.my.id/mcb-2-phase-16-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-2-ampere/">https://coveropini.my.id/mcb-2-phase-2-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-20-ampere/">https://coveropini.my.id/mcb-2-phase-20-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-25-ampere/">https://coveropini.my.id/mcb-2-phase-25-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-32-ampere/">https://coveropini.my.id/mcb-2-phase-32-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-4-ampere/">https://coveropini.my.id/mcb-2-phase-4-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-40-ampere/">https://coveropini.my.id/mcb-2-phase-40-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-50-ampere/">https://coveropini.my.id/mcb-2-phase-50-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-6-ampere/">https://coveropini.my.id/mcb-2-phase-6-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-2-phase-63-ampere/">https://coveropini.my.id/mcb-2-phase-63-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-10-ampere/">https://coveropini.my.id/mcb-3-phase-10-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-16-ampere/">https://coveropini.my.id/mcb-3-phase-16-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-2-ampere/">https://coveropini.my.id/mcb-3-phase-2-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-20-ampere/">https://coveropini.my.id/mcb-3-phase-20-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-25-ampere/">https://coveropini.my.id/mcb-3-phase-25-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-32-ampere/">https://coveropini.my.id/mcb-3-phase-32-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-4-ampere/">https://coveropini.my.id/mcb-3-phase-4-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-40-ampere/">https://coveropini.my.id/mcb-3-phase-40-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-50-ampere/">https://coveropini.my.id/mcb-3-phase-50-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-6-ampere/">https://coveropini.my.id/mcb-3-phase-6-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mcb-3-phase-63-ampere/">https://coveropini.my.id/mcb-3-phase-63-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-100-ampere/">https://coveropini.my.id/mccb-3-phase-100-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-1000-ampere/">https://coveropini.my.id/mccb-3-phase-1000-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-125-ampere/">https://coveropini.my.id/mccb-3-phase-125-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-1250-ampere/">https://coveropini.my.id/mccb-3-phase-1250-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-15-ampere/">https://coveropini.my.id/mccb-3-phase-15-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-160-ampere/">https://coveropini.my.id/mccb-3-phase-160-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-1600-ampere/">https://coveropini.my.id/mccb-3-phase-1600-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-20-ampere/">https://coveropini.my.id/mccb-3-phase-20-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-200-ampere/">https://coveropini.my.id/mccb-3-phase-200-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-25-ampere/">https://coveropini.my.id/mccb-3-phase-25-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-250-ampere/">https://coveropini.my.id/mccb-3-phase-250-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-30-ampere/">https://coveropini.my.id/mccb-3-phase-30-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-32-ampere/">https://coveropini.my.id/mccb-3-phase-32-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-320-ampere/">https://coveropini.my.id/mccb-3-phase-320-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-40-ampere/">https://coveropini.my.id/mccb-3-phase-40-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-400-ampere/">https://coveropini.my.id/mccb-3-phase-400-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-50-ampere/">https://coveropini.my.id/mccb-3-phase-50-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-500-ampere/">https://coveropini.my.id/mccb-3-phase-500-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-63-ampere/">https://coveropini.my.id/mccb-3-phase-63-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-630-ampere/">https://coveropini.my.id/mccb-3-phase-630-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-80-ampere/">https://coveropini.my.id/mccb-3-phase-80-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/mccb-3-phase-800-ampere/">https://coveropini.my.id/mccb-3-phase-800-ampere/</a><p>
+      <p><a href="https://coveropini.my.id/">https://coveropini.my.id/</a><p>
+      <p><a href="https://coveropini.my.id/sitemap/">https://coveropini.my.id/sitemap/</a><p>
       `,
     };
   },
